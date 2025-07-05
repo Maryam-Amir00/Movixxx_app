e1740928a547ba16cfef3658bf43da3d0d0cb429
Maryam Amir <herememaryamamir@gmail.com>
2025-07-06 00:45:04 +0500

    Refactor App component to include Footer and WatchlistPage, implement page toggle functionality, and wrap App in WatchlistProvider for context management.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import { useState } from 'react'
-import Navbar from './components/Navbar'
-import Home from './pages/Home'
+import React, { useState } from 'react';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Footer from './components/Footer';
+import WatchlistPage from './pages/Watchlist';
 
-function App() {
+const App = () => {
+  const [isWatchlistPage, setIsWatchlistPage] = useState(false);
+
+  const togglePage = () => {
+    setIsWatchlistPage((prev) => !prev);
+  };
 
   return (
-    <>
-      <Navbar />
-      <Home />
-    </>
-  )
-}
+    <div className="min-h-screen flex flex-col bg-[#121212]">
+      <Navbar isWatchlistPage={isWatchlistPage} onTogglePage={togglePage} />
+      
+      <main className="flex-grow">
+        {isWatchlistPage ? <WatchlistPage /> : <Home />}
+      </main>
+      
+      <Footer />
+    </div>
+  );
+};
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import {WatchlistProvider} from './context/WatchlistContext.jsx'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <WatchlistProvider>
+      <App />
+    </WatchlistProvider>
   </StrictMode>,
 )
